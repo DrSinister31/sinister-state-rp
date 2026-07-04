53da770e2e4f5a3600cf200c9c8ecbb144ed8cc1
DrSinister31 <sinisterideals31@yahoo.com>
2026-07-04 03:27:35 -0500

    Texas map blips for all 17 MLOs + key locations with proper icons

---
+++ sinister_blips/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+lua54 'yes'
+
+author 'Sinister State'
+description 'Texas-themed map blips for all buildings and MLOs'
+version '1.0.0'
+
+client_scripts {
+    'client.lua'
+}
